6fd53e6e9b6254fcb4e9b29f490892f73e90d17c
Effectivement <rnssaketo@proton.me>
2026-03-25 15:53:32 -0400

    Add printing.cpp

---
+++ printing.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World! \n";
+  cout << "HI!" << "\n";
+  cout << 3 + 9 + 89 << endl;
+  cout << "\n" << "Could this be.... A NEW LINE!?!? \n\n";
+  cout << "2 * 5: " << 2 * 5 << "\n\n";
+  
+  cout << "========" << "\n| Code |\n" << "========\n\n";
+  
+  cout << "#include <iostream> \n";
+  cout << "using namespace std; \n\n";
+  cout << "int main() {" << endl;
+  cout << "\tcout << \"Hello World! \\n\"; \n";
+  cout << "\tcout << \"HI!\" << \"\\n\"; \n";
+  cout << "\tcout << 3 + 9 + 89 << endl; \n";
+  cout << "\tcout << \"\\n\" << \"Could this be.... A NEW LINE!?!? \\n\\n\"; \n";
+  cout << "\tcout << \"2 * 5: \" << 2 * 5 << \"\\n\\n\"; \n";
+  cout << "\treturn 0; \n";
+  cout << "} \n";
+  return 0;
+}
