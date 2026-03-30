162d812e24424c088944c0aa13606b4028ccabc8
Effectivement <rnssaketo@proton.me>
2026-03-30 10:58:48 -0400

    Create commenting.cpp

---
+++ commenting.cpp
@@ -0,0 +1,17 @@
+#include <iostream> // allows us to use the stream of io!!!!!!!!!!
+using namespace std;
+
+int main() {
+    // but this is a comment, right?
+  cout << "Effective" << "\n";
+  cout << 13; // my age
+  return 0; // close function
+  
+  
+  /* This is my interesting comment–yap yap yap yap yap yap yap yap
+   yap yap yap yap yap yap yap yap yap yap yap yap yap yap yap yap yap
+    yap yap yap yap yap yap yap yap yap yap yap yap yap yap yap yap
+     yap yap yap yap yap yap yap yap yap yap yap yap yap yap yap yap
+      yap yap yap yap yap yap yap yap yap yap yap yap
+    */
+}
