0eb24952c8d46db5213696a96e533d34d2c77e48
Effectivement <rnssaketo@proton.me>
2026-04-10 18:22:17 -0400

    Create studentcard.cpp

---
+++ studentcard.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int studentID = 96833;
+  double score = 43.08;
+  char grade = 'F';
+
+  cout << "____student card___" << endl;
+  cout << "| " << endl;
+  cout << "| Hello student " << studentID << endl;
+  cout << "| Exam Score: " << score << endl;
+  cout << "| Grade: " << grade<< endl;
+  cout << "|__________________";
+
+  return 0;
+}
